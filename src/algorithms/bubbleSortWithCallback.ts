3f48dedbf9cea377d67d0f38cd466e32ddf75002
yab <hello@factions.ai>
2024-06-24 11:12:14 -0400

    multi algo handling - in progress

--- src/algorithms/bubbleSortWithCallback.ts
+++ src/algorithms/bubbleSortWithCallback.ts
@@ -4,9 +4,9 @@ const shouldSwapFn = (elem1: number, elem2: number): boolean => {
 
 export const bubbleSortStep = (
   arr: number[]
-): { arr: number[]; swapped: boolean } => {
+): { array: number[]; hasChanged: boolean } => {
   const newArray = [...arr];
-  let swapped = false;
+  let hasChanged = false;
 
   for (let i = 0; i < arr.length - 1; i++) {
     const element1 = newArray[i];
@@ -18,10 +18,10 @@ export const bubbleSortStep = (
       newArray[i] = element2;
       newArray[i + 1] = element1;
 
-      swapped = true;
+      hasChanged = true;
     }
   }
-  return { arr: newArray, swapped };
+  return { array: newArray, hasChanged };
 };
 
 // export const bubbleSortWrapped = (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,51 @@
 "use client"
 
 import { useEffect, useState } from "react";
-import { bubbleSort } from "../algorithms/bubbleSortSimple"
 import { bubbleSortStep } from "@/algorithms/bubbleSortWithCallback";
 
 const INITIAL_ARRAY = [999, 2, 4, 6, 3, 88, 1, 94, 26, 15, 89, 618, 7, 5, 13, 9]
 
+const altSortAlgo1 = bubbleSortStep
+
+const altSortAlgo2 = bubbleSortStep
+
+type algoStats = {
+  stepsTaken: number;
+  timeTaken: number;
+}
+
+const createBlankAlgoStats = (): algoStats => {
+  return {
+    stepsTaken: 0,
+    timeTaken: 0
+  };
+}
+
+
 export default function Home() {
-  const [arrays, setArrays] = useState<number[][]>([INITIAL_ARRAY])
+  const [arrays, setArrays] = useState<number[][][]>([[INITIAL_ARRAY], [INITIAL_ARRAY], [INITIAL_ARRAY]])
+
+  const [algoStats, setAlgoStats] = useState<algoStats[]>([createBlankAlgoStats(), createBlankAlgoStats(), createBlankAlgoStats()])
+
+  const algos = [bubbleSortStep, altSortAlgo1, altSortAlgo2]
+
+  // arrays[0] - bubbleSortStep
+  // arrays[1] - altSortAlgo1
+  // arrays[2] - altSortAlgo2
+
 
   // const sortedArray = bubbleSort(startingArray)
 
   useEffect(() => {
 
-    const result = bubbleSortStep(arrays[arrays.length - 1])
+    const result = bubbleSortStep(arrays[0][0])
 
-    console.log("result is", result)
+    const newArrays = [...arrays]
 
-    if (result.swapped) {
-      setTimeout(() => { setArrays([...arrays, result.arr]) }, 2000)
+    newArrays[0].unshift(result.array)
+
+    if (result.hasChanged) {
+      setTimeout(() => { setArrays(newArrays) }, 2000)
     }
 
   }, [arrays])
@@ -28,7 +55,7 @@ export default function Home() {
       <h2>Arrays</h2>
       <br />
       <div>Each array should have the same elements, but closer to the correct order:</div>
-      {arrays.map((singleArray) => {
+      {arrays[0].map((singleArray) => {
         return (
           <>
             <br />
